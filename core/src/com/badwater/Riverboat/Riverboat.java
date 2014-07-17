3585b58ba83a40166f73cf891afb9e57aeb77172
Robert Straitt <rstr5105@gmail.com>
2014-07-17 03:24:57 -0700

    Weird Edge Case, when the size changes.  But, Random River is Generating in center of map like it is supposed to.   Problem lies between lines 34 and the end of that nasty for loop.  ToDo: Refactor Nasty For Loop, to make it nicer to read, and then implement random generation of other shit.  But: overall happy with tonights progress.
    Signed-off-by: Robert Straitt <rstr5105@gmail.com>

--- core/src/com/badwater/Riverboat/Riverboat.java
+++ core/src/com/badwater/Riverboat/Riverboat.java
@@ -1,35 +1,127 @@
 package com.badwater.Riverboat;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.input.GestureDetector.GestureListener;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badwater.Riverboat.LevelMap.LevelMap;
 
-public class Riverboat extends ApplicationAdapter {
+public class Riverboat extends Game implements GestureListener {
 	SpriteBatch batch;
 	Texture img;
 	private BitmapFont font;
 	private Sprite sprite;
+	private Screen screen;
+	private OrthographicCamera camera;
+	private TiledMapRenderer renderer;
+	private float h;
+	private float w;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		h = Gdx.graphics.getHeight();
+		w = Gdx.graphics.getWidth();
+		
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, w, h);
+		TiledMap level = new LevelMap().getMap();
 		font = new BitmapFont();
+		renderer = new OrthogonalTiledMapRenderer(level);
 		font.setColor(Color.RED);
 		img = new Texture(Gdx.files.internal("badwaterlogo.png"));
+		img.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		
 		sprite = new Sprite(img);
+		sprite.setOrigin(0, 0);
+		sprite.setPosition(-sprite.getWidth() / 2, -sprite.getHeight() / 2);
+		
+		System.out.println(Gdx.graphics.getWidth());
+		System.out.println(Gdx.graphics.getHeight());
+		Gdx.input.setInputProcessor(new GestureDetector(this));
+		
 	}
-
+	
+	public void update(){
+		
+	}
+	
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		sprite.draw(batch);
-		batch.end();
+		camera.update();
+		renderer.setView(camera);
+		renderer.render();
+	}
+	
+	public void dispose(){
+		batch.dispose();
+		img.dispose();
+	}
+
+	@Override
+	public boolean touchDown(float x, float y, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean tap(float x, float y, int count, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean longPress(float x, float y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean fling(float velocityX, float velocityY, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean pan(float x, float y, float deltaX, float deltaY) {
+		// TODO Auto-generated method stub
+		camera.translate(deltaX, deltaY);
+		
+		camera.update();
+		return false;
+	}
+
+	@Override
+	public boolean panStop(float x, float y, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean zoom(float initialDistance, float distance) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2,
+			Vector2 pointer1, Vector2 pointer2) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
--- desktop/src/com/badwater/Riverboat/desktop/DesktopLauncher.java
+++ desktop/src/com/badwater/Riverboat/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.badwater.Riverboat.Riverboat;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "camera";
+		
+		config.width = 1024;
+		config.height = 768;
 		new LwjglApplication(new Riverboat(), config);
 	}
 }
