d544c83a63483b9ecd6fd34f9d087e6250e9347f
Robert Straitt <rstr5105@gmail.com>
2014-07-16 22:33:01 -0700

    Now Drawing logo to screen.  Please wait
    Signed-off-by: Robert Straitt <rstr5105@gmail.com>

--- core/src/com/badwater/Riverboat/Riverboat.java
+++ core/src/com/badwater/Riverboat/Riverboat.java
@@ -2,27 +2,34 @@ package com.badwater.Riverboat;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Riverboat extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	private BitmapFont font;
+	private Sprite sprite;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		System.out.println("Testing to see if changes get pushed by git");
+		font = new BitmapFont();
+		font.setColor(Color.RED);
+		img = new Texture(Gdx.files.internal("badwaterlogo.png"));
+		sprite = new Sprite(img);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		sprite.draw(batch);
 		batch.end();
 	}
 }
