642d65e8d0a21343ff65ec36682d2be132632a6e
Robert Straitt <rstr5105@gmail.com>
2014-07-16 21:55:40 -0700

    second initial commit

---
+++ desktop/src/com/badwater/Riverboat/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.badwater.Riverboat.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badwater.Riverboat.Riverboat;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Riverboat(), config);
+	}
+}
